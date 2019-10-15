525674ea830824bf64b74134e42313e67ec9cdee
Sam <36679647+SpoonofFlavors@users.noreply.github.com>
2019-10-15 11:31:14 -0700

    test_1

---
+++ custom/questions.js
@@ -0,0 +1,489 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+    QUESTIONS_EN_GB: [
+        {
+            'The term nibs,  refers to what in chocolate manufacturing?': [
+                'De-shelled cocoa beans ',
+                'Chunks in chocolate',
+                'Broken pieces',
+                'Burned portions',
+            ],
+        },
+        {
+            'Where would you find a cacao tree? ': [
+                'South America',
+                'United States',
+                'Finland',
+                'Germany',
+                'Canada',
+            ],
+        },
+        {
+            'Who were the first users of the cacao bean?': [
+                'The Olmecs',
+                'The Aztecs',
+                'The Romans',
+                'The Egyptians',
+            ],
+        },
+        {
+            'What did the Aztecs believe about chocolate? ': [
+                'It was an aphrodisiac',
+                'It could increase fertility',
+                'It caused skin diseases',
+                'It was poisonous',
+            ],
+        },
+        {
+            'Which King created the position, within his court, of Royal Chocolate Maker to the King?': [
+                'Louis the fourteenth',
+                'Peter the Great',
+                'Ivan the Terrible',
+                'Henry the Eighth ',
+            ],
+        },
+        {
+            'When was the first chocolate house in London opened?': [
+                '1657',
+                '1836',
+                '1887',
+                '1704',
+            ],
+        },
+        {
+            'When was milk chocolate invented?': [
+                '1875 ',
+                '1979',
+                '1763',
+                '1956',
+            ],
+        },
+        {
+            'Which ancient culture used chocolate as currency?': [
+                'Mayas',
+                'Egyptians',
+                'Romans',
+                'Chinese',
+            ],
+        },
+        {
+            'Created in October 2011, approximately, how big was the worlds largest chocolate bar?': [
+                '12 thousand  pounds',
+                '8 thousand pounds',
+                '5 thousand pounds',
+                '2 thousand pounds',
+            ],
+        },
+        {
+            'Which country eats the most chocolate per person per year?': [
+                'Switzerland',
+                'Russia',
+                'Germany',
+                'Iceland',
+            ],
+        },
+        {
+            'On average, how tall does a cocoa tree grow?': [
+                'Over 45 feet',
+                'Over 25 feet',
+                'Over 10 feet',
+                'Over 35 feet',
+            ],
+        },
+        {
+            'Which of the following items did Coenraad J. Van Houten invented?': [
+                'Chocolate press',
+                'Chocolate chips',
+                'Chocolate bunny',
+                'Chocolate bar',
+            ],
+        },
+        {
+            'Which military commander insisted that chocolate be brought on every military campaign?': [
+                'Napoleon',
+                'McArthur',
+                'Nelson',
+                'Custer',
+            ],
+        },
+        {
+            'When did Hershey opened his first factory?': [
+                '1871',
+                '1772',
+                '1717',
+                '1812',
+            ],
+        },
+
+    ],
+    QUESTIONS_EN_US: [
+        {
+            'The term nibs,  refers to what in chocolate manufacturing?': [
+                'De-shelled cocoa beans ',
+                'Chunks in chocolate',
+                'Broken pieces',
+                'Burned portions',
+            ],
+        },
+        {
+            'Where would you find a cacao tree? ': [
+                'South America',
+                'United States',
+                'Finland',
+                'Germany',
+                'Canada',
+            ],
+        },
+        {
+            'Who were the first users of the cacao bean?': [
+                'The Olmecs',
+                'The Aztecs',
+                'The Romans',
+                'The Egyptians',
+            ],
+        },
+        {
+            'What did the Aztecs believe about chocolate? ': [
+                'It was an aphrodisiac',
+                'It could increase fertility',
+                'It caused skin diseases',
+                'It was poisonous',
+            ],
+        },
+        {
+            'Which King created the position, within his court, of Royal Chocolate Maker to the King?': [
+                'Louis the fourteenth',
+                'Peter the Great',
+                'Ivan the Terrible',
+                'Henry the Eighth ',
+            ],
+        },
+        {
+            'When was the first chocolate house in London opened?': [
+                '1657',
+                '1836',
+                '1887',
+                '1704',
+            ],
+        },
+        {
+            'When was milk chocolate invented?': [
+                '1875 ',
+                '1979',
+                '1763',
+                '1956',
+            ],
+        },
+        {
+            'Which ancient culture used chocolate as currency?': [
+                'Mayas',
+                'Egyptians',
+                'Romans',
+                'Chinese',
+            ],
+        },
+        {
+            'Created in October 2011, approximately, how big was the worlds largest chocolate bar?': [
+                '12 thousand  pounds',
+                '8 thousand pounds',
+                '5 thousand pounds',
+                '2 thousand pounds',
+            ],
+        },
+        {
+            'Which country eats the most chocolate per person per year?': [
+                'Switzerland',
+                'Russia',
+                'Germany',
+                'Iceland',
+            ],
+        },
+        {
+            'On average, how tall does a cocoa tree grow?': [
+                'Over 45 feet',
+                'Over 25 feet',
+                'Over 10 feet',
+                'Over 35 feet',
+            ],
+        },
+        {
+            'Which of the following items did Coenraad J. Van Houten invented?': [
+                'Chocolate press',
+                'Chocolate chips',
+                'Chocolate bunny',
+                'Chocolate bar',
+            ],
+        },
+        {
+            'Which military commander insisted that chocolate be brought on every military campaign?': [
+                'Napoleon',
+                'McArthur',
+                'Nelson',
+                'Custer',
+            ],
+        },
+        {
+            'When did Hershey opened his first factory?': [
+                '1871',
+                '1772',
+                '1717',
+                '1812',
+            ],
+        },
+
+    ],
+  QUESTIONS_DE_DE: [
+    {
+      'Rentiere haben ein sehr dickes Fell, Wie viele Haare pro Quadratzentimeter haben sie?': [
+        '13,000',
+        '1,200',
+        '5,000',
+        '700',
+        '1,000',
+        '120,000',
+      ],
+    },
+    {
+      'Der Klassiker aus dem Jahr 1964, Rudolph mit der roten Nase, wurde gedreht in? ': [
+        'Japan',
+        'USA',
+        'Finnland',
+        'Deutschland',
+        'Kanada',
+        'Norwegen',
+        'Frankreich',
+      ],
+    },
+    {
+      'Um die Rentiere des Weihnachtsmanns kümmert sich eine der Weihnachtselfen, Wie heißt sie?': [
+        'Wunorse Openslae',
+        'Alabaster Snowball',
+        'Bushy Evergreen',
+        'Pfeffer Minstix',
+      ],
+    },
+    {
+      'Wenn alle Rentiere des Weihnachtsmanns Geweihe hätten, während sie seinen Weihnachtsschlitten ziehen, wären sie alle ': [
+        'Weiblich',
+        'Männlich',
+        'Weiblich und männlich',
+        'Kann man nicht sagen',
+      ],
+    },
+    {
+      'Was essen Rentiere?': [
+        'Flechten',
+        'Gras',
+        'Blätter',
+        'Beeren',
+      ],
+    },
+    {
+      'Welche Aussage ist nicht richtig?': [
+        'Karibus leben auf allen Kontinenten',
+        'Karibus und Rentiere gehören derselben Gattung an ',
+        'Karibus sind größer als Rentiere',
+        'Rentiere leben in Skandinavien und Russland',
+      ],
+    },
+    {
+      'In welchem Jahr kam Rudolph ins Fernsehen?': [
+        '1964',
+        '1979',
+        '2000',
+        '1956',
+      ],
+    },
+    {
+      'Wer war der Sprecher für Rudolph im klassischen Film aus dem Jahr 1964?': [
+        'Billie Mae Richards',
+        'Burl Ives',
+        'Paul Soles',
+        'Lady Gaga',
+      ],
+    },
+    {
+      'Welche Handelskette verwendete 1939 die Geschichte von Rudolph mit der roten Nase?': [
+        'Montgomery Ward',
+        'Sears',
+        'Macys',
+        'Kmart',
+      ],
+    },
+    {
+      'Wie hieß das Rentier des Weihnachtsmanns namens Donner ursprünglich?': [
+        'Dunder',
+        'Donny',
+        'Dweedle',
+        'Dreamy',
+      ],
+    },
+    {
+      'Wer hat die Geschichte von Rudolph erfunden?': [
+        'Robert May',
+        'Johnny Marks',
+        'Santa',
+        'J K  Rowling',
+      ],
+    },
+    {
+      'Wo findest du keine Rentiere?': [
+        'Nordpol',
+        'Lappland',
+        'Korvatunturi Berge',
+        'Finnland',
+      ],
+    },
+    {
+      'Warum können die Rentiere des Weihnachtsmanns fliegen?': [
+        'Magischer Staub der Rentiere',
+        'Fusion',
+        'Amanita muscaria',
+        'Elfen',
+      ],
+    },
+    {
+      'Wieviele Rentierhufe gibt es hier einschließlich Rudolph?': [
+        '36',
+        '24',
+        '16',
+        '8',
+      ],
+    },
+    {
+      'Der Weihnachtsmann hat nur ein weibliches Rentier, Wie heißt es?': [
+        'Blitzen',
+        'Clarice',
+        'Cupid',
+        'Cupid',
+      ],
+    },
+    {
+      'Wie war der Name des erzählenden Schneemanns im klassischen Film Rudolph mit der roten Nase aus dem Jahr 1964?': [
+        'Sam',
+        'Frosty',
+        'Burl',
+        'Snowy',
+      ],
+    },
+    {
+      'Wie hieß der Vater von Rudolph?': [
+        'Donner',
+        'Dasher',
+        'Blixen',
+        'Comet',
+      ],
+    },
+    {
+      'Wie war der Name des Trainers der Rentierspiele im klassischen Film aus dem Jahr 1964?': [
+        'Comet',
+        'Blixen',
+        'Donner',
+        'Dasher',
+      ],
+    },
+    {
+      'Wie war im klassichen Film aus 1964 der Name des Hirsches, mit dem sich Rudolph befreundete?': [
+        'Fireball',
+        'Clarice',
+        'Jumper',
+        'Vixen',
+      ],
+    },
+    {
+      'Wie hat der Vater von Rudolph, Donner, im Film aus dem Jahr 1964 versucht, die Nase von Rudolph zu verbergen?': [
+        'Schwarzer Schlamm',
+        'Sack',
+        'Kissenbezug',
+        'Socke',
+      ],
+    },
+    {
+      'Was möchte die Misfit Elfe im Film aus dem Jahr 1964 werden anstatt eine Elfe für den Weihnachtsmann?': [
+        'Zahnarzt',
+        'Rentier',
+        'Spielzeugmacher',
+        'Kerzenmacher',
+      ],
+    },
+    {
+      'Was war die einzige Schwäche von Bumble im Film aus dem Jahr 1964?': [
+        'Konnte nicht schwimmen',
+        'War immer hungrig',
+        'Zuckerstangen',
+        'Schielte',
+      ],
+    },
+    {
+      'Was sucht Yukon Cornelius in Wirklichkeit im Film aus dem Jahr 1964?': [
+        'Pfefferminz',
+        'Gold',
+        'Indien',
+        'Polarbären',
+      ],
+    },
+    {
+      'Warum befindet sich der Zug im Film aus dem Jahr 1964 auf der Insel des fehlerhaften Spielzeugs?': [
+        'Viereckige Räder',
+        'Keine Dampfmaschine',
+        'Farbe stimmt nicht',
+        'Pfeift nicht',
+      ],
+    },
+    {
+      'Wie lautet der Name des Schachtelmännchens im Film aus dem Jahr 1964?': [
+        'Charlie',
+        'Sam',
+        'Billy',
+        'Jack',
+      ],
+    },
+    {
+      'Warum hat der Weihnachtsmann im Film aus dem Jahr 1964 Weihnachten beinahe abgesagt?': [
+        'Sturm',
+        'Kein Schnee',
+        'Kein Spielzeug',
+        'Die Rentiere waren krank',
+      ],
+    },
+    {
+      'Welches tierische Geräusch machte die Elfe im Film aus dem Jahr 1964, um den Bumble abzulenken?': [
+        'Oink',
+        'Knurr',
+        'Wauwau',
+        'Miau',
+      ],
+    },
+    {
+      'Wie lautet der Name des Goldsuchers im Film aus dem Jahr 1964?': [
+        'Yukon Cornelius',
+        'Slider Sam',
+        'Bumble',
+        'Jack',
+      ],
+    },
+    {
+      'Wie weit ziehen Rentiere auf ihren Wanderungen?': [
+        '4800 km',
+        '1100 km',
+        '800 km',
+        '0 km',
+      ],
+    },
+    {
+      'Wie schnell läuft ein Rentier?': [
+        '77 km pro Stunde',
+        '27 km pro Stunde',
+        '30 km pro Stunde',
+        '22 km pro Stunde',
+        '83 km pro Stunde',
+        '65 km pro Stunde',
+      ],
+    },
+  ],
+};
